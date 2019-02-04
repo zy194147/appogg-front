0e1e4e6032eb5378a278fa9ba71a70d969b1d3b0
zy194147 <zy194147@126.com>
2019-02-04 22:00:51 +0800

    修复跨域bug

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,178 @@
 <template>
+
+
+
   <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
     <FormItem prop="user">
-      <Input type="text" v-model="formInline.user" placeholder="Username">
-      <Icon type="ios-person-outline" slot="prepend"></Icon>
-      </Input>
-    </FormItem>
-    <br/>
-    <FormItem prop="password">
-      <Input type="password" v-model="formInline.password" placeholder="Password">
-      <Icon type="ios-locked-outline" slot="prepend"></Icon>
-      </Input>
+
+      <Menu mode="horizontal" :theme="theme1" active-name="1">
+        <MenuItem name="1">
+          <Icon type="ios-paper" />
+          文章
+        </MenuItem>
+        <MenuItem name="2">
+          <Icon type="ios-people" />
+          破解
+        </MenuItem>
+        <Submenu name="3">
+          <template slot="title">
+            <Icon type="ios-stats" />
+            软件
+
+          </template>
+          <!--<MenuGroup title="使用">-->
+            <MenuItem v-for="d in listdata">{{ d.id }}</MenuItem>
+            <!--<MenuItem name="3-2">iphone</MenuItem>-->
+            <!--<MenuItem name="3-3">windows</MenuItem>-->
+          <!--</MenuGroup>-->
+          <!--<MenuGroup title="留存">-->
+            <!--<MenuItem name="3-4">linux</MenuItem>-->
+            <!--<MenuItem name="3-5">mac</MenuItem>-->
+            <!--<MenuItem name="3-6">其他</MenuItem>-->
+          <!--</MenuGroup>-->
+        </Submenu>
+        <MenuItem name="4">
+          <Icon type="ios-construct" />
+          我的地盘
+        </MenuItem>
+      </Menu>
     </FormItem>
     <br/>
     <FormItem>
-      <Button type="primary" @click="handleSubmit('formInline')">登录</Button>
+      <!--<Button type="primary" @click="handleSubmit('formInline')">登录</Button>-->
+      <!--<Button type="primary" @click="handleSubmit('formInline')">登录</Button>-->
+      <!--<Button type="primary" @click="handleSubmit('formInline')">登录</Button>-->
+      <!--<Button type="primary" @click="handleSubmit('formInline')">登录</Button>-->
     </FormItem>
+
+
+
+    <FormItem>
+      <Card style="width:350px;float: left;margin:20px;">
+        <p slot="title">
+          <Icon type="ios-film-outline"></Icon>
+          Classic film
+        </p>
+        <a href="#" slot="extra" @click.prevent="changeLimit">
+          <Icon type="ios-loop-strong"></Icon>
+          Change
+        </a>
+        <ul style="list-style: none">
+          <li v-for="item in randomMovieList">
+            <a :href="item.url" target="_blank">{{ item.name }}</a>
+            <span>
+                    <Icon type="ios-star" v-for="n in 4" :key="n"></Icon><Icon type="ios-star" v-if="item.rate >= 9.5"></Icon><Icon type="ios-star-half" v-else></Icon>
+                    {{ item.rate }}
+                </span>
+          </li>
+        </ul>
+      </Card>
+      <Card style="width:350px;float: left;margin:20px;">
+        <p slot="title">
+          <Icon type="ios-film-outline"></Icon>
+          Classic film
+        </p>
+        <a href="#" slot="extra" @click.prevent="changeLimit">
+          <Icon type="ios-loop-strong"></Icon>
+          Change
+        </a>
+        <ul style="list-style: none">
+          <li v-for="item in randomMovieList">
+            <a :href="item.url" target="_blank">{{ item.name }}</a>
+            <span>
+                    <Icon type="ios-star" v-for="n in 4" :key="n"></Icon><Icon type="ios-star" v-if="item.rate >= 9.5"></Icon><Icon type="ios-star-half" v-else></Icon>
+                    {{ item.rate }}
+                </span>
+          </li>
+        </ul>
+      </Card>
+      <Card style="width:350px;float: left;margin:20px;">
+        <p slot="title">
+          <Icon type="ios-film-outline"></Icon>
+          Classic film
+        </p>
+        <a href="#" slot="extra" @click.prevent="changeLimit">
+          <Icon type="ios-loop-strong"></Icon>
+          Change
+        </a>
+        <ul style="list-style: none">
+          <li v-for="item in randomMovieList">
+            <a :href="item.url" target="_blank">{{ item.name }}</a>
+            <span>
+                    <Icon type="ios-star" v-for="n in 4" :key="n"></Icon><Icon type="ios-star" v-if="item.rate >= 9.5"></Icon><Icon type="ios-star-half" v-else></Icon>
+                    {{ item.rate }}
+                </span>
+          </li>
+        </ul>
+      </Card>
+      <Card style="width:350px;float: left;margin:20px;">
+        <p slot="title">
+          <Icon type="ios-film-outline"></Icon>
+          Classic film
+        </p>
+        <a href="#" slot="extra" @click.prevent="changeLimit">
+          <Icon type="ios-loop-strong"></Icon>
+          Change
+        </a>
+        <ul style="list-style: none">
+          <li v-for="item in randomMovieList">
+            <a :href="item.url" target="_blank">{{ item.name }}</a>
+            <span>
+                    <Icon type="ios-star" v-for="n in 4" :key="n"></Icon><Icon type="ios-star" v-if="item.rate >= 9.5"></Icon><Icon type="ios-star-half" v-else></Icon>
+                    {{ item.rate }}
+                </span>
+          </li>
+        </ul>
+      </Card>
+      <Card style="width:350px;float: left;margin:20px;">
+        <p slot="title">
+          <Icon type="ios-film-outline"></Icon>
+          Classic film
+        </p>
+        <a href="#" slot="extra" @click.prevent="changeLimit">
+          <Icon type="ios-loop-strong"></Icon>
+          Change
+        </a>
+        <ul style="list-style: none">
+          <li v-for="item in randomMovieList">
+            <a :href="item.url" target="_blank">{{ item.name }}</a>
+            <span>
+                    <Icon type="ios-star" v-for="n in 4" :key="n"></Icon><Icon type="ios-star" v-if="item.rate >= 9.5"></Icon><Icon type="ios-star-half" v-else></Icon>
+                    {{ item.rate }}
+                </span>
+          </li>
+        </ul>
+      </Card>
+      <Card style="width:350px;float: left;margin:20px;">
+        <p slot="title">
+          <Icon type="ios-film-outline"></Icon>
+          Classic film
+        </p>
+        <a href="#" slot="extra" @click.prevent="changeLimit">
+          <Icon type="ios-loop-strong"></Icon>
+          Change
+        </a>
+        <ul style="list-style: none">
+          <li v-for="item in randomMovieList">
+            <a :href="item.url" target="_blank">{{ item.name }}</a>
+            <span>
+                    <Icon type="ios-star" v-for="n in 4" :key="n"></Icon><Icon type="ios-star" v-if="item.rate >= 9.5"></Icon><Icon type="ios-star-half" v-else></Icon>
+                    {{ item.rate }}
+                </span>
+          </li>
+        </ul>
+      </Card>
+    </FormItem>
+
   </Form>
+
+
+
+
 </template>
 <script>
 
+  import axios from 'axios'
   import Httpservice from '@/router/service'
 
   export default {
@@ -39,6 +193,60 @@
         },
         http:Httpservice.getAxios,
         listdata:[],
+        theme1: 'light',
+        movieList: [
+          {
+            name: 'The Shawshank Redemption',
+            url: 'https://movie.douban.com/subject/1292052/',
+            rate: 9.6
+          },
+          {
+            name: 'Leon:The Professional',
+            url: 'https://movie.douban.com/subject/1295644/',
+            rate: 9.4
+          },
+          {
+            name: 'Farewell to My Concubine',
+            url: 'https://movie.douban.com/subject/1291546/',
+            rate: 9.5
+          },
+          {
+            name: 'Forrest Gump',
+            url: 'https://movie.douban.com/subject/1292720/',
+            rate: 9.4
+          },
+          {
+            name: 'Life Is Beautiful',
+            url: 'https://movie.douban.com/subject/1292063/',
+            rate: 9.5
+          },
+          {
+            name: 'Spirited Away',
+            url: 'https://movie.douban.com/subject/1291561/',
+            rate: 9.2
+          },
+          {
+            name: 'Schindlers List',
+            url: 'https://movie.douban.com/subject/1295124/',
+        rate: 9.4
+    },
+      {
+        name: 'The Legend of 1900',
+          url: 'https://movie.douban.com/subject/1292001/',
+        rate: 9.2
+      },
+      {
+        name: 'WALL·E',
+          url: 'https://movie.douban.com/subject/2131459/',
+        rate: 9.3
+      },
+      {
+        name: 'Inception',
+          url: 'https://movie.douban.com/subject/3541415/',
+        rate: 9.2
+      }
+    ],
+      randomMovieList: [],
       }
     },
     methods: {
@@ -50,18 +258,58 @@
             this.$Message.error('表单验证失败!')
           }
         })
+      },
+      getData() {
+        console.log("开始")
+
+
+        axios.get('/api/hello/say')
+          .then((response) => {
+            console.log(1)
+            console.log(response.data.data,"hahahahahaha",response.data.status);
+
+
+            if(response.data.status === 200){
+              console.log("yes")
+              this.listdata = response.data.data
+              console.log(this.listdata,"1111111111111")
+            } else {
+              console.log("no")
+
+              this.listdata = []
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+        console.log("结束")
+      },
+
+      changeLimit () {
+        function getArrayItems(arr, num) {
+          const temp_array = [];
+          for (let index in arr) {
+            temp_array.push(arr[index]);
+          }
+          const return_array = [];
+          for (let i = 0; i<num; i++) {
+            if (temp_array.length>0) {
+              const arrIndex = Math.floor(Math.random()*temp_array.length);
+              return_array[i] = temp_array[arrIndex];
+              temp_array.splice(arrIndex, 1);
+            } else {
+              break;
+            }
+          }
+          return return_array;
+        }
+        this.randomMovieList = getArrayItems(this.movieList, 5);
       }
     },
     created() {
-      this.http.get('/hello/say',{withCredentials:true}).then((response) => {
-        let res = response.data
-        console.log(response.data,".............")
-        if(res.status === 200){
-          this.listdata = res.data
-        } else {
-          this.listdata = []
-        }
-      })
+
+      this.getData();
+      this.changeLimit();
     }
   }
 </script>
